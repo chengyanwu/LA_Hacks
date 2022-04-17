f9ac3b56e5dc190c46b13a1987506eb851706562
chengyanwu <cheng-yan@ucsb.edu>
2022-04-16 19:59:00 -0700

    set up the detection program on MAX78000

---
+++ Car_Detection/build/all_imgs.d
@@ -0,0 +1,2 @@
+/Users/ianwu/Desktop/SpotOn/MAX78000_SDK/Examples/MAX78000/CNN/cats-dogs_demo/build/all_imgs.o: \
+ TFT/evkit/all_imgs.c
---
+++ Car_Detection/build/startup_max78000.d
@@ -0,0 +1,2 @@
+/Users/ianwu/Desktop/SpotOn/MAX78000_SDK/Examples/MAX78000/CNN/cats-dogs_demo/build/startup_max78000.o: \
+ ../../../../Libraries/CMSIS/Device/Maxim/MAX78000/Source/GCC/startup_max78000.S
